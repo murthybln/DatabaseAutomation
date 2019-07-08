a689a01d7fbb22c839b702218eb981b3a5d38025
Narasimha Bandaru (CES Ltd) <narasimhab@Computechcorp.net>
2019-07-08 15:35:10 +0530

    JIRA DATA-0001: Removed unneccesary code

--- query.py
+++ query.py
@@ -1,7 +1,5 @@
 db_conn1 = "DRIVER={SQL SERVER};server=192.168.18.36;database=Maverick;uid=narasimha;pwd=Welcome321;"
 
-db_conn = "DRIVER={SQL SERVER};server=192.168.18.36;database=Maverick;Trusted_Connection=yes;"
 query_fail = """SELECT * FROM dbo.Employee"""
-
 
 query_pass = """SELECT * FROM dbo.Employee where aGE = 21"""
